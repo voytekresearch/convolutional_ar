edb61ecb5f48e0f9990a8541b412b4ba68c94a83
Ryan Hammonds <ryanphammonds@gmail.com>
2024-02-11 13:05:27 -0800

    1d reshape

--- convolutional_ar/reshape.py
+++ convolutional_ar/reshape.py
@@ -1,4 +1,4 @@
-"""Reshape an image into an autregressive problem."""
+"""Reshape an image or vector into an autoregressive problem."""
 
 import numpy as np
 import torch
@@ -29,4 +29,25 @@ def ar_reshape(x: np.ndarray, order: int) -> (np.ndarray, np.ndarray):
     x_ar = torch.from_numpy(x_ar.astype(np.float32))
     y_ar = torch.from_numpy(y_ar.astype(np.float32))
 
+    return x_ar, y_ar
+
+def ar_reshape_1d(x: np.ndarray, order: int) -> (np.ndarray, np.ndarray):
+    """Lag a vector of values to create an autoregressive problem.
+
+    Parameters
+    ----------
+    x : 1d array
+        Vector values.
+    order : int
+        Number of points to lag.
+
+    Returns
+    -------
+    x_ar : 2d array
+        Past values, e.g (x_{i}, x_{i+1}... x_{i + order}).
+    y_ar : 1d array
+        Future values, e.g. (x_{i + order + 1}).
+    """
+    x_ar = np.lib.stride_tricks.sliding_window_view(x, order, axis=0)[:-1]
+    y_ar = x[order:]
     return x_ar, y_ar
